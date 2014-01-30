4819853752aba525d93827736655914374ea810f
Allan Crooks <allan@amcone.net>
2014-01-30 15:59:27 +0000

    Removed unfinished rubbish from utils.

--- pyinthesky/utils.py
+++ pyinthesky/utils.py
@@ -9,16 +9,3 @@ def nstag(tree, tag):
 		return tag # No schema.
 	schema = roottag.split('}', 1)[0] + '}'
 	return '{0}{1}'.format(schema, tag)
-
-# Think about this
-def strepr(obj, idfmt, module=False):
-	clsname = obj.__class__.__name__
-	
-	if module:
-	    import pyinthesky
-	    if getattr(pyinthesky, clsname, None) is obj.__class__:
-		    clsname = 'pyinthesky.' + clsname
-	    else:
-			clsname = obj.__class__.__module__ + '.' + clsname
-		    
-	return '<{0}({1}'
