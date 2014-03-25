ff8d50f93ac15fde4c034bcdd50a26ca406815ec
Allan Crooks <allan@amcone.net>
2014-03-25 22:38:44 +0000

    Moved code that bound accessiblity methods to objects to the methods module, as well as adding support for class assignment.
    We do some dynamic class construction too. Pretty happy with that feature. :)

--- pyinthesky/main.py
+++ pyinthesky/main.py
@@ -1,59 +1,21 @@
 from .base import Connection as _ConnectionBase
-from .methods import method_sig_wrapper as mkfunc, make_ambiguous_function as ambig
+from .methods import bind_service_methods
 from functools import partial
 
 class Connection(_ConnectionBase):
     
     def connect(self):
         _ConnectionBase.connect(self)
-
-        # Build up method registries for the connection.
-        conn_methods = {}
-
+        all_services = []
         for device in self.devices.values():
-            
-            # Build up method registries for the device too.
-            dev_methods = {}
-            
-            # Process methods for each service.
-            for service in device.services.values():
-                self._connect_service(service)
-                for m, f in service.method_registry.items():
-                    
-                    # Populate parent method registries.
-                    dev_methods.setdefault(m, []).append(
-                        [f, '.'.join(('device', service.name, m))])
-                    conn_methods.setdefault(m, []).append(
-                        [f, '.'.join(('conn', device.devtype, service.name, m))])
-                        
+            services = device.services.values()
+            for service in services:
+                service.connect()
+                bind_service_methods(service, bind_to_class=service.name + 'Service')
                 setattr(self, service.name, service)
-                setattr(device, service.name, service)
-                    
-            # Bind aliases to device objects.
-            for methname, methods in dev_methods.items():
-                if len(methods) == 1:
-                    setattr(device, methname, methods[0][0])
-                else:
-                    f = ambig(methname, [x[1] for x in methods])
-                    setattr(device, methname, f)
-                    
+            bind_service_methods(device, services, bind_to_class=device.devtype + 'Device')
+            all_services.extend(services)
             setattr(self, device.devtype, device)
-       
-        # Bind aliases to this connection object.
-        for methname, methods in conn_methods.items():
-            if len(methods) == 1:
-                setattr(self, methname, methods[0][0])
-            else:
-                f = ambig(methname, [x[1] for x in methods])
-                setattr(self, methname, f)
-                    
-    def _connect_service(self, service):
-        service.connect()
-        service.method_registry = {}
-        for methname, (in_args, out_args) in service.methods.items():
-            target = partial(service.invoke, methname)
-            f = mkfunc(target, methname, in_args.argument_order, in_args.argument_defaults)
-            service.method_registry[methname] = f
-            setattr(service, methname, f)
+        bind_service_methods(self, all_services, bind_to_class=True)
 
     # XXX: Need a __str__ and a __repr__
--- pyinthesky/methods.py
+++ pyinthesky/methods.py
@@ -3,7 +3,10 @@
 #     positionally. True for all, or sequence for specific ordering.
 #  2. Support for *args
 #  3. Support for **kwargs.
-def method_sig_wrapper(target, name, varnames, defaults=None):
+#
+# Should be called func_sig_wrapper
+def method_sig_wrapper(target, name, varnames, defaults=None,
+    make_method=False):
     
     # First of all, defaults will need to be a tuple rather than a
     # dictionary.
@@ -31,8 +34,16 @@ def method_sig_wrapper(target, name, varnames, defaults=None):
     
     del defaults
     
-    argstr = ', '.join(varnames)
-    wrapfunc = 'def {name}({argstr}): return _target_(**locals())\n'
+    if make_method:
+        argstr = ', '.join(['self'] + list(varnames))
+        wrapfunc = (
+            "def {name}({argstr}): __locals__ = locals(); "
+            "__locals__.pop('self'); return _target_(**__locals__)\n"
+        )
+    else:
+        argstr = ', '.join(varnames)
+        wrapfunc = 'def {name}({argstr}): return _target_(**locals())\n'
+    
     wrapfunc = wrapfunc.format(**vars())
     wrapcode = compile(wrapfunc, '<generated_function>', 'exec')
     global_ns = {}
@@ -55,3 +66,70 @@ def make_ambiguous_function(name, func_locations):
     describes = '\n'.join(['  - ' + fl for fl in func_locations])
     cant_do_it.__doc__ = _ambiguous_docstring + describes
     return cant_do_it
+    
+# XXX: Add docstrings...
+def bind_service_methods(target, services=None, bind_to_class=False):
+    if services is None:
+        services = [target]
+
+    from pyinthesky import meta
+    bind_to_meta = bind_target_class = False
+    
+    if bind_to_class is True:
+        target_class = target.__class__
+    elif bind_to_class is False:
+        target_class = None
+    elif hasattr(meta, bind_to_class):
+        target_class = getattr(meta, bind_to_class)
+        bind_target_class = True
+    else:
+        target_class = type(bind_to_class, (target.__class__,), {})
+        target_class.__module__ = 'pyinthesky.meta'
+        bind_to_meta = True
+        bind_target_class = True
+
+    from functools import partial
+    serv_methods = {}
+    for service in services:
+        for methname, (in_args, out_args) in service.methods.items():
+            func = partial(service.invoke, methname)
+            args = list(in_args.argument_order)
+            f = method_sig_wrapper(func, methname, args,
+                in_args.argument_defaults,
+                make_method=target_class is not None)
+            serv_methods.setdefault(methname, []).append([
+                f, service.name + '.' + methname,
+            ])
+            
+    # We've now built functions for all remote methods, so see what we
+    # can set and what we can't.
+    from types import MethodType
+    for methname, methods in serv_methods.items():
+        if len(methods) == 1:
+            f = methods[0][0]
+        else:
+            f = make_ambiguous_function(methname, [x[1] for x in methods])
+        if target_class is None:
+            setattr(target, methname, f)
+        else:
+            setattr(target_class, methname, MethodType(f, None, target_class))
+
+	if bind_target_class:
+		target.__class__ = target_class
+    if bind_to_meta:
+        setattr(meta, target_class.__name__, target_class)
+    return dict((k, len(serv_methods[k]) == 1) for k in serv_methods)
+
+def build_service_methods(service, as_class_methods=False):
+    if not hasattr(service, methods):
+        raise ValueError('cannot pass unconnected service object')
+
+    method_registry = {}
+    for methname, (in_args, out_args) in service.methods.items():
+        if as_class_methods:
+            target = partial(service.__class__.invoke, action_name=methname)    
+        else:
+            target = partial(service.invoke, methname)
+        f = mkfunc(target, methname, in_args.argument_order, in_args.argument_defaults)
+        method_registry[methname] = f
+    return method_registry
