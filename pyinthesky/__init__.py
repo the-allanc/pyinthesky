0269205aa0156899f32824dd0768c6d78f462412
Allan Crooks <allan@amcone.net>
2014-05-10 15:15:34 +0100

    Define a dictionary of all service types - indicating if they are mandatory or optional (there is a new service, "SkyRemote", which will only be available on some boxes).

--- pyinthesky/__init__.py
+++ pyinthesky/__init__.py
@@ -1,1 +1,7 @@
 from transport import Transport
+
+SERVICE_TYPES = {
+    'urn:schemas-nds-com:device:SkyControl:2': True,
+    'urn:schemas-nds-com:device:SkyServe:2': True,
+    'urn:schemas-nds-com:device:SkyRemote:2': False,
+}
